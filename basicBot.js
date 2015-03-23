8be59773db656f9781343adaa25f689afc54723c
GaabrieeLp <gabrielf.possamai@gmail.com>
2015-03-23 16:33:26 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -662,7 +662,7 @@
                             	$("#room-settings").css("display", "none");
                             	 //toggle.click();
                             	setTimeout(function (){$(".dj-cycle > .off").click();}, 600);
-                    		setTimeout(function (){$("#room-settings").css("display", "block");}, 700);
+                            	setTimeout(function (){$("#room-settings").css("display", "block");}, 700);
                     		setTimeout(function (){$("#room-name").click();}, 800);
                             }
                         }, basicBot.settings.cycleMaxTime * 60 * 1000);
